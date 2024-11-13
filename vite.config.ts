a54b2fd075e5e406cf10d25f6400da4e40daf58e
Stefan Schramm <mail@stefanschramm.net>
2024-11-13 09:37:54 +0100

    Minify HTML

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // static content is expected in public/ (for example data.json)
 
 export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+      template: 'index.html',
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
